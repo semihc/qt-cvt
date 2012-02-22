0aa9355958951d9a7986920f31ea93c5d1a1d4bf
Semih Cemiloglu <semih.cemiloglu@gmail.com>
2012-02-22 23:41:49 +1100

    introduce PRJS_HOME env. variable

--- setenv.sh
+++ setenv.sh
@@ -2,6 +2,11 @@
 # Necessary environment variables to work QmkBld based projects
 #
 
+# If Projects Home directory is not defined
+if [ -z "$PRJS_HOME" ]; then
+  PRJS_HOME=$HOME
+fi
+
 # QmkBld related variables
 QMKBLD=$HOME/qmkbld
 QMK_DEBUG=0
@@ -9,5 +14,7 @@ QMAKEFEATURES=$QMKBLD/prf
 export QMKBLD QMK_DEBUG QMAKEFEATURES
 
 # The project that uses QmkBld 
-export PRJ_ROOT=$HOME/qt-cvt
+export PRJ_ROOT=$PRJS_HOME/qt-cvt
 
+# Give feedback about project root
+echo "PRJ_ROOT=$PRJ_ROOT"
